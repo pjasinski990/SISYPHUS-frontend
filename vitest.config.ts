0f0cd677863f8405d3378a93aa709f970b944c1b
Piotr Jasinski <piotr.jasinski990@gmail.com>
2024-10-22 19:38:41 +0200

    track coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,13 @@ export default defineConfig({
         environment: 'jsdom',
         setupFiles: './vitest.setup.ts',
         include: ['test/**/*.test.{ts,tsx}', 'componentTest/**/*.test.{ts,tsx}'],
+        coverage: {
+            provider: 'v8',
+            reportsDirectory: './coverage',
+            reporter: ['text', 'json', 'html'],
+            all: true,
+            include: ['src/**/*.{ts,tsx}'],
+        },
     },
     resolve: {
         alias: {
